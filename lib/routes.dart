9a9ce33bc92877ff9f5d628ecf7fc532f8f86a59
Dal Yoon <dalyoon@Dals-MacBook-Pro.local>
2020-01-07 17:22:59 +0900

    add routes

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import "./screens/home.dart";
+
+class Routes {
+  static final home = "/";
+
+  static final router = {
+    home: (context) => Home(),
+  };
+}
--- lib/widgets/App.dart
+++ lib/widgets/App.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
 
-import "../screens/Home.dart";
+import "../routes.dart";
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Web Zzigmuk',
-        theme: ThemeData(brightness: Brightness.dark),
-        home: Home());
+      title: 'Flutter Web Zzigmuk',
+      theme: ThemeData(brightness: Brightness.dark),
+      initialRoute: Routes.home,
+      routes: Routes.router);
 }
